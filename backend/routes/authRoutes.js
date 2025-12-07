ad8c00e1416c132e8b99ad5585f5f68729a9cdc8
kaibad <badukailash001@gmail.com>
2025-12-07 12:56:18 +0545

    feat: add validation middleware for auth endpoints

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,34 @@
 import express from "express";
+import { body } from "express-validator";
+
 import { registerUser, loginUser } from "../controllers/authController.js";
+import { validateRequest } from "../middleware/ValidateMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post(
+  "/register",
+  [
+    body("name").notEmpty().withMessage("Name Required"),
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password should be minimum of 6 characters"),
+  ],
+  validateRequest,
+  registerUser
+);
+router.post(
+  "/login",
+  [
+    [
+      body("email").isEmail().withMessage("Invalid Email"),
+      body("password").notEmpty().withMessage("Password Required"),
+    ],
+    validateRequest,
+  ],
+  validateRequest,
+  loginUser
+);
 
 export default router;
